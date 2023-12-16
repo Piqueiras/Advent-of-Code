06efa0829c67985e6000afa1fa6092ebdbfa9838
Daniel <danipiquerasvalle@gmail.com>
2023-12-16 17:32:03 +0100

    Day 16 2023

---
+++ 2023/16/one.nim
@@ -0,0 +1,108 @@
+import strutils,sequtils,sets
+
+let text =  open("beams.txt").readAll.split("\n")
+let SIZE = text.len
+
+type Direction = enum Up, Left, Down, Right
+
+type Coords = array[2,int]
+
+type Beam = tuple[loc:Coords,dir:Direction] 
+
+template `[]`(matrix: seq[string], idx: array[2, int]): char = matrix[idx[0]][idx[1]]
+
+proc advance(beam:var Beam) =
+    case beam.dir:
+        of Up:
+            dec beam.loc[0]
+        of Down:
+            inc beam.loc[0]
+        of Left:
+            dec beam.loc[1]
+        of Right:
+            inc beam.loc[1]
+
+proc advance(beams:var seq[Beam]) =
+    var i = 0
+    while i<beams.len:
+        let beam = beams[i]
+        #Horizontal beam finds a |
+        if beam.dir in {Left,Right} and text[beam.loc]=='|':
+            beams.insert(beam,i) #Clone it
+            beams[i].dir = Up
+            beams[i].advance
+            beams[i+1].dir = Down
+            beams[i+1].advance
+            i+=2
+            continue
+        #Vertical beam finds a -
+        if beam.dir in {Up,Down} and text[beam.loc]=='-':
+            beams.insert(beam,i) #Clone it
+            beams[i].dir = Left
+            beams[i].advance
+            beams[i+1].dir = Right
+            beams[i+1].advance
+            i+=2
+            continue
+        #Encounters a \
+        if text[beam.loc]=='\\':
+            case beam.dir:
+                of Up:
+                    beams[i].dir = Left
+                of Down:
+                    beams[i].dir = Right
+                of Left:
+                    beams[i].dir = Up
+                of Right:
+                    beams[i].dir = Down
+            beams[i].advance
+            inc i
+            continue
+        #Encounters a \
+        if text[beam.loc]=='/':
+            case beam.dir:
+                of Up:
+                    beams[i].dir = Right
+                of Down:
+                    beams[i].dir = Left
+                of Left:
+                    beams[i].dir = Down
+                of Right:
+                    beams[i].dir = Up
+            beams[i].advance
+            inc i
+            continue
+        #Else
+        beams[i].advance
+        inc i
+    #Check boundaries
+    beams.keepItIf(it.loc[0] in 0..SIZE-1 and it.loc[1] in 0..SIZE-1)
+
+proc echoEnerguized(energuized:HashSet[Coords]) =
+    for i,line in text:
+        var output = line
+        for j,c in output.mpairs:
+            if [i,j] in energuized:
+                c = '#'
+        echo output
+
+let initial : Beam = ([0,0],Right)
+
+var beams = @[initial]
+
+var energized = initHashSet[Coords]()
+
+#Must avoid cycles
+var visited = initHashSet[Beam]()
+
+while beams.len>0:
+    beams.keepItIf(it notin visited)
+    for beam in beams:
+        visited.incl(beam)
+        energized.incl(beam.loc)
+    beams.advance
+    # discard readLine(stdin)
+    # echo beams
+    # echoEnerguized(energized)
+   
+echo energized.len
